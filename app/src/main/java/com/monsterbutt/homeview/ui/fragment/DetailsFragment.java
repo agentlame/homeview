d3b870ae0759c693915f72717bb5c3046efaa785
drewhill80@gmail.com <drewhill80@gmail.com>
2017-01-10 21:25:27 -0500

    fix background changing in going from one detail view to the next

--- app/src/main/java/com/monsterbutt/homeview/ui/fragment/DetailsFragment.java
+++ app/src/main/java/com/monsterbutt/homeview/ui/fragment/DetailsFragment.java
@@ -119,6 +119,12 @@ public class DetailsFragment extends android.support.v17.leanback.app.DetailsFra
         mSelectionHandler = new CardSelectionHandler(this, this, this, mServer, mItem, img);
 
         mBackgroundURL = getActivity().getIntent().getStringExtra(DetailsActivity.BACKGROUND);
+        if (!mBackgroundURL.startsWith("http")) {
+            if (mServer.getServerURL().endsWith("/") && mBackgroundURL.startsWith("/"))
+                mBackgroundURL = mServer.getServerURL() + mBackgroundURL.substring(1);
+            else
+                mBackgroundURL = mServer.getServerURL() + mBackgroundURL;
+        }
         if (!TextUtils.isEmpty(mBackgroundURL))
             mSelectionHandler.updateBackground(mBackgroundURL, true);
         mLifeCycleMgr.put(CardSelectionHandler.key, mSelectionHandler);
